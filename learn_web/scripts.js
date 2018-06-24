078cfb4476af0b610db0ce3c6361cce1406c8d80
Sproodigy <sproodigy@gmail.com>
2018-06-24 19:06:27 +0400

    Added iframe chapter

---
+++ learn_web/scripts.js
@@ -0,0 +1,55 @@
+function bordersCollapse() {
+  document.getElementById('collapse').style.borderCollapse = "collapse";
+  document.getElementById('table-title').innerHTML = '<span id="blue" style="padding-left: 1em;">&lt;<span id="brown">table </span><span id="crimson">style</span><span>=</span>"width: 90%; margin-left: 4em; border: 2px solid; border-collapse: collapse;"&gt;</span>';
+}
+
+function returning() {
+  document.getElementById('collapse').style.borderCollapse = "separate";
+  document.getElementById('collapse').style.borderSpacing = "1em";
+  document.getElementById('table-title').innerHTML = '<span id="blue" style="padding-left: 1em;">&lt;<span id="brown">table </span><span id="crimson">style</span><span>=</span>"width: 90%; margin-left: 4em; border: 2px solid; border-spacing: 1em;"&gt;</span>';
+}
+
+function disc() {
+  document.getElementById('diff-list').style.listStyleType = "disc";
+    document.getElementById('diff-types').innerHTML = '<span id="blue">&lt;<span id="brown">ul</span><span id="crimson"> style</span><span>=</span>"list-style-type: disk"&gt;</span>'
+}
+
+function circle() {
+  document.getElementById('diff-list').style.listStyleType = "circle";
+  document.getElementById('diff-types').innerHTML = '<span id="blue">&lt;<span id="brown">ul</span><span id="crimson"> style</span><span>=</span>"list-style-type: circle"&gt;</span>'
+}
+
+function square() {
+  document.getElementById('diff-list').style.listStyleType = "square";
+    document.getElementById('diff-types').innerHTML = '<span id="blue">&lt;<span id="brown">ul</span><span id="crimson"> style</span><span>=</span>"list-style-type: square"&gt;</span>'
+}
+
+function none() {
+  document.getElementById('diff-list').style.listStyleType = "none";
+  document.getElementById('diff-types').innerHTML = '<span id="blue">&lt;<span id="brown">ul</span><span id="crimson"> style</span><span>=</span>"list-style-type: none"&gt;</span>'
+}
+
+function one() {
+  document.getElementById('diff-ord-list').type = "1";
+  document.getElementById('diff-ord-types').innerHTML = '<span id="blue">&lt;<span id="brown">ol</span><span id="crimson"> type</span><span>=</span>"1"&gt;</span>'
+}
+
+function A() {
+  document.getElementById('diff-ord-list').type = "A";
+  document.getElementById('diff-ord-types').innerHTML = '<span id="blue">&lt;<span id="brown">ol</span><span id="crimson"> type</span><span>=</span>"A"&gt;</span>'
+}
+
+function a() {
+  document.getElementById('diff-ord-list').type = "a";
+  document.getElementById('diff-ord-types').innerHTML = '<span id="blue">&lt;<span id="brown">ol</span><span id="crimson"> type</span><span>=</span>"a"&gt;</span>'
+}
+
+function I() {
+  document.getElementById('diff-ord-list').type = "I";
+  document.getElementById('diff-ord-types').innerHTML = '<span id="blue">&lt;<span id="brown">ol</span><span id="crimson"> type</span><span>=</span>"I"&gt;</span>'
+}
+
+function i() {
+  document.getElementById('diff-ord-list').type = "i";
+  document.getElementById('diff-ord-types').innerHTML = '<span id="blue">&lt;<span id="brown">ol</span><span id="crimson"> type</span><span>=</span>"i"&gt;</span>'
+}
