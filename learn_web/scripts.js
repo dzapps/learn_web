ac9f49e9b5446fd9adcf4ca3c546a807ad5723a7
Sproodigy <sproodigy@gmail.com>
2018-07-08 20:23:48 +0400

    Added a few chapters

--- learn_web/scripts.js
+++ learn_web/scripts.js
@@ -11,45 +11,77 @@ function returning() {
 
 function disc() {
   document.getElementById('diff-list').style.listStyleType = "disc";
-    document.getElementById('diff-types').innerHTML = '<span class="blue">&lt;<span class="brown">ul</span><span class="crimson"> style</span><span>=</span>"list-style-type: disk"&gt;</span>'
+    document.getElementById('diff-types').innerHTML = '<span class="blue">&lt;<span class="brown">ul</span><span class="crimson"> style</span><span>=</span>"list-style-type: disk"&gt;</span>';
 }
 
 function circle() {
   document.getElementById('diff-list').style.listStyleType = "circle";
-  document.getElementById('diff-types').innerHTML = '<span class="blue">&lt;<span class="brown">ul</span><span class="crimson"> style</span><span>=</span>"list-style-type: circle"&gt;</span>'
+  document.getElementById('diff-types').innerHTML = '<span class="blue">&lt;<span class="brown">ul</span><span class="crimson"> style</span><span>=</span>"list-style-type: circle"&gt;</span>';
 }
 
 function square() {
   document.getElementById('diff-list').style.listStyleType = "square";
-    document.getElementById('diff-types').innerHTML = '<span class="blue">&lt;<span class="brown">ul</span><span class="crimson"> style</span><span>=</span>"list-style-type: square"&gt;</span>'
+    document.getElementById('diff-types').innerHTML = '<span class="blue">&lt;<span class="brown">ul</span><span class="crimson"> style</span><span>=</span>"list-style-type: square"&gt;</span>';
 }
 
 function none() {
   document.getElementById('diff-list').style.listStyleType = "none";
-  document.getElementById('diff-types').innerHTML = '<span class="blue">&lt;<span class="brown">ul</span><span class="crimson"> style</span><span>=</span>"list-style-type: none"&gt;</span>'
+  document.getElementById('diff-types').innerHTML = '<span class="blue">&lt;<span class="brown">ul</span><span class="crimson"> style</span><span>=</span>"list-style-type: none"&gt;</span>';
 }
 
 function one() {
   document.getElementById('diff-ord-list').type = "1";
-  document.getElementById('diff-ord-types').innerHTML = '<span class="blue">&lt;<span class="brown">ol</span><span class="crimson"> type</span><span>=</span>"1"&gt;</span>'
+  document.getElementById('diff-ord-types').innerHTML = '<span class="blue">&lt;<span class="brown">ol</span><span class="crimson"> type</span><span>=</span>"1"&gt;</span>';
 }
 
 function A() {
   document.getElementById('diff-ord-list').type = "A";
-  document.getElementById('diff-ord-types').innerHTML = '<span class="blue">&lt;<span class="brown">ol</span><span class="crimson"> type</span><span>=</span>"A"&gt;</span>'
+  document.getElementById('diff-ord-types').innerHTML = '<span class="blue">&lt;<span class="brown">ol</span><span class="crimson"> type</span><span>=</span>"A"&gt;</span>';
 }
 
 function a() {
   document.getElementById('diff-ord-list').type = "a";
-  document.getElementById('diff-ord-types').innerHTML = '<span class="blue">&lt;<span class="brown">ol</span><span class="crimson"> type</span><span>=</span>"a"&gt;</span>'
+  document.getElementById('diff-ord-types').innerHTML = '<span class="blue">&lt;<span class="brown">ol</span><span class="crimson"> type</span><span>=</span>"a"&gt;</span>';
 }
 
 function I() {
   document.getElementById('diff-ord-list').type = "I";
-  document.getElementById('diff-ord-types').innerHTML = '<span class="blue">&lt;<span class="brown">ol</span><span class="crimson"> type</span><span>=</span>"I"&gt;</span>'
+  document.getElementById('diff-ord-types').innerHTML = '<span class="blue">&lt;<span class="brown">ol</span><span class="crimson"> type</span><span>=</span>"I"&gt;</span>';
 }
 
 function i() {
   document.getElementById('diff-ord-list').type = "i";
-  document.getElementById('diff-ord-types').innerHTML = '<span class="blue">&lt;<span class="brown">ol</span><span class="crimson"> type</span><span>=</span>"i"&gt;</span>'
+  document.getElementById('diff-ord-types').innerHTML = '<span class="blue">&lt;<span class="brown">ol</span><span class="crimson"> type</span><span>=</span>"i"&gt;</span>';
+}
+
+function standart() {
+  document.getElementById('cars').size = "1";
+  document.getElementById('cars').multiple = "";
+  document.getElementById('saab_select').selected = "";
+  document.getElementById('cars_desc').innerHTML = '<span class="blue">&lt;</span><span class="brown">select </span><span class="crimson">name</span><span>=</span><span class="blue">"cars"&gt;</span>';
+  document.getElementById('saab_select_desc').innerHTML = '<span class="blue">&lt;</span><span class="brown">option </span><span class="crimson">value</span><span>=</span><span class="blue">"saab"&gt;<span>Saab</span><span class="blue">&lt;</span><span class="brown">/option</span>&gt;</span>';
+}
+
+function size() {
+  document.getElementById('cars').size = "3";
+  document.getElementById('cars').multiple = "";
+  document.getElementById('saab_select').selected = "";
+  document.getElementById('cars_desc').innerHTML = '<span class="blue">&lt;</span><span class="brown">select </span><span class="crimson">name</span><span>=</span><span class="blue">"cars" </span><span class="crimson">size</span><span>=</span><span class="blue">"3"&gt;</span>';
+  document.getElementById('saab_select_desc').innerHTML = '<span class="blue">&lt;</span><span class="brown">option </span><span class="crimson">value</span><span>=</span><span class="blue">"saab"&gt;<span>Saab</span><span class="blue">&lt;</span><span class="brown">/option</span>&gt;</span>';
+}
+
+function selected() {
+  document.getElementById('cars').size = "3";
+  document.getElementById('cars').multiple = "";
+  document.getElementById('saab_select').selected = "selected";
+  document.getElementById('cars_desc').innerHTML = '<span class="blue">&lt;</span><span class="brown">select </span><span class="crimson">name</span><span>=</span><span class="blue">"cars" </span><span class="crimson">size</span><span>=</span><span class="blue">"3"&gt;</span>';
+  document.getElementById('saab_select_desc').innerHTML = '<span class="blue">&lt;</span><span class="brown">option </span><span class="crimson">value</span><span>=</span><span class="blue">"saab" </span><span class="crimson">selected</span><span>=</span><span class="blue">"selected"&gt;<span>Saab</span><span class="blue">&lt;</span><span class="brown">/option</span>&gt;</span>';
+}
+
+function multiple() {
+  document.getElementById('cars').size = "3";
+  document.getElementById('cars').multiple = "multiple";
+  document.getElementById('saab_select').selected = "selected";
+  document.getElementById('cars_desc').innerHTML = '<span class="blue">&lt;</span><span class="brown">select </span><span class="crimson">name</span><span>=</span><span class="blue">"cars" </span><span class="crimson">size</span><span>=</span><span class="blue">"3"</span><span class="crimson">multiple</span><span>=</span><span class="blue">"multiple"&gt;</span>';
+  document.getElementById('saab_select_desc').innerHTML = '<span class="blue">&lt;</span><span class="brown">option </span><span class="crimson">value</span><span>=</span><span class="blue">"saab" </span><span class="crimson">selected</span><span>=</span><span class="blue">"selected"&gt;<span>Saab</span><span class="blue">&lt;</span><span class="brown">/option</span>&gt;</span>';
 }
