32985c34371d001969714511ffabbe9306822efd
Sproodigy <sproodigy@gmail.com>
2018-09-26 22:20:12 +0400

    Little work

--- learn_web/scripts/cssPropertiesA.js
+++ learn_web/scripts/cssPropertiesA.js
@@ -2,13 +2,10 @@ function changeStateSingle(id, state) {
   document.getElementById(id).style.display = state;
 }
 
-function changeVisib(id) {
-  let elemsarr = document.getElementsByClassName('visibility');
-  length = elemsarr.length;
-  for (i=0; i<=length; i++) {
-    document.getElementById(id).style.display = "flex";
-    elemsarr[i].style.display = "none";
-  }
+function changeVisibAndState(id1, id2, state1, state2) {
+  document.getElementById(id1).style.alignContent = state1;
+  document.getElementById(id1).style.WebkitAlignContent = state1;
+  document.getElementById(id2).style.display = state2;
 }
 
 function alignItems() {
