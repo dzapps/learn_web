72adcde30bcf6e20d8f8bffab0106ea205985dc7
Sproodigy <sproodigy@gmail.com>
2018-09-27 21:28:20 +0400

    First property completed

--- learn_web/scripts/cssPropertiesA.js
+++ learn_web/scripts/cssPropertiesA.js
@@ -6,6 +6,12 @@ function changeVisibAndState(id1, id2, state1, state2) {
   document.getElementById(id1).style.alignContent = state1;
   document.getElementById(id1).style.WebkitAlignContent = state1;
   document.getElementById(id2).style.display = state2;
+
+  let text = document.getElementsByClassName('text');
+  len = text.length;
+  for (let i=0; i<=len; i++) {
+    text[i].innerHTML = state1;
+  }
 }
 
 function alignItems() {
