c8f139b2879c10bdadbe8fb3a706c4dc8ae10dfb
Sproodigy <sproodigy@gmail.com>
2018-10-12 21:48:31 +0400

    Added a few chapters.

--- learn_web/scripts/cssPropertiesB.js
+++ learn_web/scripts/cssPropertiesB.js
@@ -28,7 +28,7 @@ function backfaceVisib(imgId, imgState, text) {
   document.getElementById(`${imgId}`).style.transform = 'rotateY(180deg)';
 
   document.getElementsByClassName('text')[0].innerHTML = 'rotateY(180deg);';
-  document.getElementsByClassName('text')[1].innerHTML = text;
+  document.getElementsByClassName('text')[1].innerHTML = text + ';';
 }
 
 function frontSide() {
@@ -40,12 +40,12 @@ function backgrAttach(id, attach, blend, clip, color, image, origin, position, r
   let bodyImgState = document.body.style;
   let doc = document.getElementById(id).style;
   let changeText = document.getElementsByClassName('text');
-  changeText[2].innerHTML = attach;
+  changeText[2].innerHTML = attach + ';';
   if (backgrImage) {
     bodyImgState.backgroundImage = backgrImage;
     bodyImgState.backgroundAttachment = attach;
     changeText[0].innerHTML = 'body ';
-    changeText[1].innerHTML = backgrImage;
+    changeText[1].innerHTML = backgrImage + ';';
   }
   else {
     doc.backgroundAttachment = attach;
@@ -67,8 +67,8 @@ function rabbit() {
   document.body.style.backgroundImage = 'url("../images/rabbit_on_the_grass.jpeg")';
   document.body.style.backgroundAttachment = 'fixed';
   changeText[0].innerHTML = 'body ';
-  changeText[1].innerHTML = "url('../images/rabbit_on_the_grass.jpeg')";
-  changeText[2].innerHTML = 'fixed';
+  changeText[1].innerHTML = "url('../images/rabbit_on_the_grass.jpeg');";
+  changeText[2].innerHTML = 'fixed;';
 }
 
 function blend(id, blend, img, secImg) {
@@ -78,4 +78,25 @@ function blend(id, blend, img, secImg) {
   doc.backgroundBlendMode = blend;
   doc.backgroundImage = img + ', ' + secImg;
   doc.backgroundRepeat = 'no-repeat, repeat';
+
+  changeText[0].innerHTML = "url('../images/vasserman_big.jpg'), url('../images/background_img.jpeg');";
+  changeText[1].innerHTML = 'no-repeat, repeat;';
+  changeText[2].innerHTML = blend + ';';
+}
+
+function clip(id, clip) {
+  let doc = document.getElementById(id).style;
+  let changeText = document.getElementsByClassName('text');
+
+  doc.backgroundClip = clip;
+  doc.backgroundColor = 'blue';
+
+  changeText[0].innerHTML = 'blue;';
+  changeText[1].innerHTML = clip + ';';
+}
+
+function color(id, color) {
+  document.getElementById(id).style.backgroundColor = color;
+
+  document.getElementsByClassName('text')[0].innerHTML = color + ';';
 }
