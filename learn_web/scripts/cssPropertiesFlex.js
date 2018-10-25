b25b1f36e343271d220b72cb0c2796a09dbfc603
Sproodigy <sproodigy@gmail.com>
2018-10-25 21:29:52 +0400

    One chapter added.

--- learn_web/scripts/cssPropertiesFlex.js
+++ learn_web/scripts/cssPropertiesFlex.js
@@ -48,3 +48,9 @@ function flexWrap(id, wrap) {
   document.getElementById(id).style.flexWrap = wrap;
   document.getElementsByClassName('text')[0].innerHTML = wrap + ';';
 }
+
+function caretColor(id, color) {
+  document.getElementById(id).style.caretColor = color;
+  document.getElementById(id).value = `Caret color is ${color}`;
+  document.getElementsByClassName('text')[0].innerHTML = color + ';';
+}
