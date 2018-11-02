8fbf103e7195f7537072bde0db65b29fad3cda5f
Sproodigy <sproodigy@gmail.com>
2018-11-02 15:13:20 +0400

    One property added.

--- learn_web/scripts/font.js
+++ learn_web/scripts/font.js
@@ -1,9 +1,14 @@
-function font(id, font) {
+function font (id, font) {
   document.getElementById(id).style.font = font
   document.getElementsByClassName('text')[0].innerHTML = font
 }
 
-function fontFamily(id, family) {
+function fontFamily (id, family) {
   document.getElementById(id).style.fontFamily = family
   document.getElementsByClassName('text')[0].innerHTML = family
+}
+
+function fontKerning (id, kerning) {
+  document.getElementById(id).style.kerning = kerning
+  document.getElementsByClassName('text')[0].innerHTML = kerning
 }
