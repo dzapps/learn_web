a253afba716c00a28e0aa827e133c687f8bd2c78
Sproodigy <sproodigy@gmail.com>
2018-11-06 22:16:12 +0400

    One chapter added.

--- learn_web/scripts/list.js
+++ learn_web/scripts/list.js
@@ -1,5 +1,27 @@
-function listStyleImg(id, listStyleImg) {
-  document.getElementById(id).style.listStyleImage = listStyleImg;
-  document.getElementsByClassName('text')[0].innerHTML = listStyleImg;
-  // document.getElementsByClassName('text')[1].innerHTML = listStyleImg;
+function listStyleImg(idUl, listTag, listStyleImg) {
+  if (listTag == 'ul') {
+    document.getElementsByTagName('li')[0].style.listStyleImage = listStyleImg;
+    document.getElementsByTagName('li')[1].style.listStyleImage = listStyleImg;
+    document.getElementsByTagName('li')[2].style.listStyleImage = listStyleImg;
+
+    document.getElementsByClassName('text')[0].innerHTML = listTag;
+    document.getElementsByClassName('text')[1].innerHTML = listStyleImg;
+  } else if (listTag == '.cake') {
+    document.getElementsByTagName('li')[0].style.listStyleImage = listStyleImg;
+    document.getElementsByTagName('li')[1].style.listStyleImage = 'none';
+    document.getElementsByTagName('li')[2].style.listStyleImage = 'none';
+
+    document.getElementsByClassName('text')[0].innerHTML = listTag;
+    document.getElementsByClassName('text')[1].innerHTML = listStyleImg;
+  }
+}
+
+function listStylePos(id, listStylePos) {
+  document.getElementById(id).style.listStylePosition = listStylePos;
+  document.getElementsByClassName('text')[0].innerHTML = listStylePos;
+}
+
+function listStyleType(id, listStyleType) {
+  document.getElementById(id).style.listStyleType = listStyleType;
+  document.getElementsByClassName('text')[0].innerHTML = listStyleType;
 }
