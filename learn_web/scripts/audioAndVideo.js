4af675242285d9b210775cd9987b79c322eeed07
Sproodigy <sproodigy@gmail.com>
2018-11-25 13:05:18 +0400

    Added a few pages.

--- learn_web/scripts/audioAndVideo.js
+++ learn_web/scripts/audioAndVideo.js
@@ -1,4 +1,3 @@
-
 function audioPreload(id, preload) {
   document.getElementById(id).preload = preload;
   document.getElementsByClassName("replace")[0].innerHTML = preload;
@@ -7,4 +6,14 @@ function audioPreload(id, preload) {
 function audioSrc(id, src) {
   document.getElementById(id).src = src;
   document.getElementsByClassName("replace")[0].innerHTML = src;
+}
+
+function videoHeight(id, height) {
+  document.getElementById(id).style.height = height;
+  document.getElementsByClassName("replace")[0].innerHTML = height;
+}
+
+function videoWidth(id, width) {
+  document.getElementById(id).style.width = width;
+  document.getElementsByClassName("replace")[0].innerHTML = width;
 }
---
+++ learn_web/scripts/formsAndInput.js
@@ -0,0 +1,20 @@
+
+function audioPreload(id, preload) {
+  document.getElementById(id).preload = preload;
+  document.getElementsByClassName("replace")[0].innerHTML = preload;
+}
+
+function audioSrc(id, src) {
+  document.getElementById(id).src = src;
+  document.getElementsByClassName("replace")[0].innerHTML = src;
+}
+
+function videoHeight(id, height) {
+  document.getElementById(id).style.height = height;
+  document.getElementsByClassName("replace")[0].innerHTML = height;
+}
+
+function videoWidth(id, width) {
+  document.getElementById(id).style.width = width;
+  document.getElementsByClassName("replace")[0].innerHTML = width;
+}
