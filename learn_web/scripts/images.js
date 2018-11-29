4359c2acfc0b38accf1ed2a03afd50701b1d6a68
Sproodigy <sproodigy@gmail.com>
2018-11-29 22:31:12 +0400

    Added a few pages.

---
+++ learn_web/scripts/images.js
@@ -0,0 +1,10 @@
+// function highlightText(cls) {
+//   let text = document.getElementsByClassName(cls);
+//   let len = text.length;
+//
+//   for (let j=0; j<=len; j++) {
+//     text[j].style.textShadow = '0 0 2px rgba(122, 7, 66, 0.86), 0 0 4px rgba(100, 0, 0, 0.7)';
+//   }
+// }
+//
+// highlightText('highlightText');
