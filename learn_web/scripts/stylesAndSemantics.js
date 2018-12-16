db0ecd5ff09c6dd973c12265db6516b27bb1685e
Sproodigy <sproodigy@gmail.com>
2018-12-16 20:25:38 +0400

    Added a few attributes.

---
+++ learn_web/scripts/stylesAndSemantics.js
@@ -0,0 +1,4 @@
+function changeDialogOpen() {
+  document.getElementById('dialogOpenExamp').open = true;
+  // document.getElementsByClassName('replaceDialogOpen')[0].innerHTML = '"' + text + '"';
+}
