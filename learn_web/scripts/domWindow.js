0e6377736e5d05ec819275d650c90c3dfe6a2dc2
Sproodigy <sproodigy@gmail.com>
2019-01-08 02:02:26 +0400

    Added a few attributes.

--- learn_web/scripts/domWindow.js
+++ learn_web/scripts/domWindow.js
@@ -1,6 +1,5 @@
 var myWindow;
 
-
 function openWindow() {
   myWindow = window.open("", "myWindow", "width=400, height=200");
 }
@@ -19,4 +18,27 @@ function checkWindow() {
     document.getElementById('windowClosedP').innerHTML = 'The window was open.';
     document.getElementsByClassName('replace')[0].innerHTML = 'The window was open.'
   }
+}
+
+function checkFrameElement() {
+  let frame = window.frameElement;
+  console.log(frame);
+
+  if (frame) {
+    frame.src = "https://www.w3schools.com/";
+  } else {
+    document.getElementById('windowFrameElementP').innerHTML = 'Frames don\'t exist.';
+    document.getElementsByClassName('replace')[0].innerHTML = 'Frames don\'t exist.';
+  }
+}
+
+function replaceFrameData() {
+  window.frames[0].location = "https://liquimoly.ru/";
+  document.getElementsByClassName('replace')[0].innerHTML = '"https://liquimoly.ru/"';
+}
+
+function showInnerHeight() {
+  var h = window.innerHeight;
+  document.getElementById("windowInnerHeightP").innerHTML = "Inner height: " + h;
+  document.getElementsByClassName("replace")[0].innerHTML = "Inner height: " + h;
 }
